564c7584797ddf1f6fdb8bcc214bc9ba1c554268
athiefaine <arnaud.thiefaine@gmail.com>
2024-05-06 18:31:36 +0200

    Explaining variables and constants (import)

--- src/test/java/org/athiefaine/tidying/explain_v_c/now/OrderLineTest.java
+++ src/test/java/org/athiefaine/tidying/explain_v_c/now/OrderLineTest.java
@@ -1,6 +1,5 @@
 package org.athiefaine.tidying.explain_v_c.now;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
